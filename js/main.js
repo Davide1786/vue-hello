7d90dcd042b5724ec5bf8f03d111dabe7291a9b3
Davide <dadavidenapolitano@gmail.com>
2021-09-06 16:12:17 +0200

    Esercizio completato

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+Vue.config.devtools =  true;
+
+const app = new Vue({
+   el: '#root',
+   data: {
+      message: 'BOOLEAN - Classe #39!',
+      benvenuto: 'Buongiorno a tutti',
+   }
+ })
